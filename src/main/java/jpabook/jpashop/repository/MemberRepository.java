9244e2d1590a956d5d56be1095a2cb604df2feb5
codemyungja <codemyungja@naver.com>
2024-12-11 16:30:33 +0900

    회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,15 +3,19 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor //final 필드에 대한 매개변수 생성자 작성 -> 매개변수 생성자에 @Autowired가 적용 -> 생성자에서 EntityManager 주입 가능
+//단, @Autowired로 EntityManager를 주입하는 것은 SpringBoot Data JPA를 쓰고 있을 때 가능한 것임
+//원래는 @PersistenceContext로 EntityManager를 의존성 주입해야 한다.
 public class MemberRepository {
 
-    @PersistenceContext //스프링이 자동 설정한 JPA의 EntityManager를 주입하도록 함
-    private EntityManager em;
+    //@PersistenceContext //스프링이 자동 설정한 JPA의 EntityManager를 주입하도록 함
+    private final EntityManager em;
 
 /*
     @PersistenceUnit //스프링이 자동 설정한 JPA의 EntityManagerFactory를 주입 (스프링 내에서는 거의 사용X)
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //데이터 조회 시에 JPA의 성능을 최적화해준다.
+// org.springframework.transaction.annotation에 있는 @Transactional 어노테이션을 권장 : 사용 가능 옵션이 더 많기 때문
+@RequiredArgsConstructor //final 필드만을 가지고 매개변수 생성자 작성해주는 어노테이션 -> 생성자에서 final 필드에 대한 의존성 주입이 이루어짐
+public class MemberService {
+    
+    //final : 컴파일 시점에서 주입이 잘 되었는지 확인 가능하므로 사용 권장
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional //JPA에서 데이터 변경은 무조건 @Transactional 안에서 수행되어야 함(원칙)
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증(비지니스 로직)
+        memberRepository.save(member); //영속성 컨텍스트에 member 객체를 저장할 때, PK 필드가 키 값이 된다. 따라서 (DB에 저장하기 이전에) PK 필드에 값이 채워진다.
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
