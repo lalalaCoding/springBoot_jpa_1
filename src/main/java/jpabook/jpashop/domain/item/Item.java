f73b3be59a3f26e1e44597bb6444bc3d2324f627
codemyungja <codemyungja@naver.com>
2024-12-12 16:22:09 +0900

    상품 관련 기능 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==//
+    //도메인 주도 개발 : 재고수량을 필드로 갖는 클래스 내부에 재고와 관련된 비지니스 로직을 구현한다.
+    public void addStock(int quantity) { //재고 증가
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) { //재고 감소
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { // 신규로 생성한 Item 객체인 경우 (영속성 컨텍스트에 동일한 Item 객체가 없음) -> 등록 해야함.
+            em.persist(item);
+        } else { //DB에 등록된 Item 객체가 영속성 컨텍스트에 들어있는 경우 -> 수정 해야함.
+            em.merge(item); // 추후 설명
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
